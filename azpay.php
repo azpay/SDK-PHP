ba4b5fd3dececf153c86968cc5079f5aeece4a49
Gabriel Guerreiro <gabrielguerreiro@azclick.com.br>
2015-03-06 18:38:06 -0300

    Exceptions

--- azpay.php
+++ azpay.php
@@ -2,6 +2,10 @@
 /**
  * AZPay SDK
  * 
+ * Version: 1.0.0
+ * Author: Gabriel Guerreiro
+ * Copyright AZClick
+ * 
  */
 
 include 'config.php';
@@ -10,19 +14,33 @@ include 'xml_requests.php';
 
 class AZPay {
 
-	# Key and ID of the client
+	/**
+	 * VERSION
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Client Key and Client ID
+	 * @var array
+	 */
 	public $merchant = array(
 		'id' => '',
 		'key' => ''
 	);
 
-	# Order
+	/**
+	 * Order Details
+	 * @var array
+	 */
 	public $config_order = array(
 		'reference' => '',
 		'totalAmount' => ''
 	);
 
-	# Billing
+	/**
+	 * Billing Details
+	 * @var array
+	 */
 	public $config_billing = array(
 		'customerIdentity' => '1',
 		'name' => 'Fulano de Tal',
@@ -36,7 +54,10 @@ class AZPay {
 		'email' => 'fulanodetal@email.com'
 	);
 
-	# Card Payment
+	/**
+	 * Creditcard data
+	 * @var array
+	 */
 	public $config_card_payments = array(
 		'acquirer' => '1',
 		'method' => '1',
@@ -57,7 +78,10 @@ class AZPay {
 		'tokenCard' => ''
 	);
 
-	# Boleto
+	/**
+	 * Boleto configuration
+	 * @var array
+	 */
 	public $config_boleto = array(
 		'acquirer' => '10',
 		'expire' => '',
@@ -68,7 +92,10 @@ class AZPay {
 		'instructions' => ''
 	);
 
-	# Options
+	/**
+	 * Options extra
+	 * @var array
+	 */
 	public $config_options = array(
 		'urlReturn' => '',
 		'fraud' => 'false',
@@ -76,28 +103,64 @@ class AZPay {
 	);
 
 
+	/**
+	 * Reponse
+	 * @var [type]
+	 */
 	public $curl_response;
-	public $curl_response_error;
-	public $times = 0;
+
+	/**
+	 * Meta data
+	 * @var [type]
+	 */
+	public $curl_response_meta;
+
+	/**
+	 * Errors
+	 * @var [type]
+	 */
+	public $curl_error;	
+
+	/**
+	 * Errors Code
+	 * @var [type]
+	 */
+	public $curl_error_code;	
+
+	/**
+	 * Error flag
+	 * @var boolean
+	 */
 	public $error = false;
+
+	/**
+	 * Flag to execute Exceptions
+	 * @var boolean
+	 */
+	public $throw_exceptions = true;
+
 
 
 	/**
 	 * Construct class
-	 * @param [type] $merchant_id  [description]
-	 * @param [type] $merchant_key [description]
+	 * 
+	 * @param [type] $merchant_id  [Client ID]
+	 * @param [type] $merchant_key [Client Key]
 	 */
 	function __construct($merchant_id, $merchant_key) {
 
+		if (!function_exists('curl_init'))
+            throw new Exception('CURL module not available! Pest requires CURL. See http://php.net/manual/en/book.curl.php');        
+
 		$this->merchant['id'] = $merchant_id;
 		$this->merchant['key'] = $merchant_key;
-
 	}
 
 
 	/**
 	 * Execute request
-	 * @return [type] [description]
+	 * 
+	 * @return [type] [execute cUrl request]
 	 */
 	public function execute($xml) {		
 
@@ -108,27 +171,35 @@ class AZPay {
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-		if (curl_errno($ch)) {
-			
-			$this->curl_response_error = curl_errno($ch) . ' - ' . curl_error($ch);
+		$this->curl_response = curl_exec($ch);
+		$this->curl_response_meta = curl_getinfo($ch);
+
+		if ($this->curl_response === false || $this->curl_response_meta === false) {
 			$this->error = true;
-
-		} else {
-
-			$this->curl_response = curl_exec($ch);
-			curl_close($ch);
-
+			$this->curl_error = curl_error($ch);
+			$this->curl_error_code = curl_errno($ch);
 		}
 
+		if ($this->curl_response === false && $this->throw_exceptions)			
+			throw new AZPay_Curl_Exec_Exception(curl_error($ch));		
+
+		if ($this->curl_response_meta === false && $this->throw_exceptions)
+			throw new AZPay_Curl_Meta_Exception(curl_error($ch));
+
+		curl_close($ch);
+
+		$this->checkErrors();
 	}
 
 
 	/**
-	 * [responseXml description]
+	 * Return the cUrl response 
+	 * 
 	 * @return [type] [description]
 	 */
 	public function response() {
 
+		// If no errors, return XML parsed
 		if ($this->error == false) {
 			
 			$xml = simplexml_load_string($this->curl_response);
@@ -137,7 +208,7 @@ class AZPay {
 
 		} else {
 
-			return $this->response_error;
+			return $this->curl_error;
 
 		}
 
@@ -146,7 +217,7 @@ class AZPay {
 
 	/**
 	 * Request authorization to complete the transaction
-	 * with authorization
+	 * 	 
 	 * @return [type]          [description]
 	 */
 	public function authorize() {
@@ -161,7 +232,9 @@ class AZPay {
 	}
 
 	/**
-	 * Request the capture of the transaction, after authorization
+	 * Request the capture of the transaction
+	 * after the authorization request, to payment validation
+	 * 
 	 * @return [type] [description]
 	 */
 	public function capture($transactionId) {
@@ -178,12 +251,9 @@ class AZPay {
 
 	/**
 	 * Request Sale
-	 * without authorization
-	 * @param  [type] $order    [description]
-	 * @param  [type] $payments [description]
-	 * @param  [type] $billing  [description]
-	 * @param  [type] $options  [description]
-	 * @return [type]           [description]
+	 * without a pre authorization
+	 * 
+	 * @return [String] [description]
 	 */
 	public function sale() {
 
@@ -199,6 +269,8 @@ class AZPay {
 
 	/**
 	 * Request Report
+	 * to check the transaction status
+	 * 
 	 * @param  [type] $tid [TID]
 	 * @return [type]      [description]
 	 */
@@ -216,6 +288,7 @@ class AZPay {
 
 	/**
 	 * Request Cancel
+	 * 
 	 * @param  [type] $tid [description]
 	 * @return [type]      [description]
 	 */
@@ -233,6 +306,7 @@ class AZPay {
 
 	/**
 	 * Request Boleto
+	 * 
 	 * @return [type] [description]
 	 */
 	public function boleto() {
@@ -246,5 +320,92 @@ class AZPay {
 
 	}
 
+	/**
+	 * Check error exceptions
+	 * 
+	 * @return [void] [description]
+	 */
+	private function checkErrors() {
+
+		if (!$this->throw_exceptions)
+            return;
+
+        $meta = $this->curl_response_meta;
+        $response = $this->curl_response;
+
+        if ($meta === false)
+            return;
+
+        switch ($meta['http_code']) {
+            case 400:
+                throw new AZPay_BadRequest($response);
+                break;
+            case 401:
+                throw new AZPay_Unauthorized($response);
+                break;
+            case 403:
+                throw new AZPay_Forbidden($response);
+                break;
+            case 404:
+                throw new AZPay_NotFound($response);
+                break;
+            case 405:
+                throw new AZPay_MethodNotAllowed($response);
+                break;
+            case 409:
+                throw new AZPay_Conflict($response);
+                break;
+            case 410:
+                throw new AZPay_Gone($response);
+                break;
+            case 422:
+                throw new AZPay_InvalidRecord($response);
+                break;
+            default:
+                if ($meta['http_code'] >= 400 && $meta['http_code'] <= 499) {
+                    throw new AZPay_ClientError($response);
+                } elseif ($meta['http_code'] >= 500 && $meta['http_code'] <= 599)
+                    throw new AZPay_ServerError($response); elseif (!isset($meta['http_code']) || $meta['http_code'] >= 600) {
+                    throw new AZPay_UnknownResponse($response);
+                }
+
+        }
+
+	}
+
 }
+
+
+/**
+ * General Exceptions
+ */
+class AZPay_Exception extends Exception {}
+class AZPay_Unknown_Exception extends AZPay_Exception {}
+
+/**
+ * Client Exception
+ */
+class AZPay_Client_Exception extends AZPay_Exception {}
+
+/**
+ * HTTP Exceptions
+ */
+class AZPay_BadRequest extends AZPay_Client_Exception {}
+class AZPay_Unauthorized extends AZPay_Client_Exception {}
+class AZPay_Forbidden extends AZPay_Client_Exception {}
+class AZPay_NotFound extends AZPay_Client_Exception {}
+class AZPay_MethodNotAllowed extends AZPay_Client_Exception {}
+class AZPay_Conflict extends AZPay_Client_Exception {}
+class AZPay_Gone extends AZPay_Client_Exception {}
+class AZPay_InvalidRecord extends AZPay_Client_Exception {}
+class AZPay_ServerError extends AZPay_Client_Exception {}
+class AZPay_UnknownResponse extends AZPay_Client_Exception {}
+
+/**
+ * cUrl Exceptions
+ */
+class AZPay_Curl_Init_Exception extends AZPay_Exception {}
+class AZPay_Curl_Meta_Exception extends AZPay_Exception {}
+class AZPay_Curl_Exec_Exception extends AZPay_Exception {}
+
 ?>
